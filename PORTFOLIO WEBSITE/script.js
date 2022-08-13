04ac9c40784e3f6eaf3db41c202675f39e92aa7b
rushikesh0412 <68814000+rushikesh0412@users.noreply.github.com>
2022-08-13 14:07:35 +0530

    Add files via upload

---
+++ PORTFOLIO WEBSITE/script.js
@@ -0,0 +1,16 @@
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        setTimeout(() =>{
+            document.querySelector('.ham').style.display = 'inline'
+        }, 350);
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(() =>{
+            document.querySelector('.cross').style.display = 'inline'
+        }, 50);
+    }
+})
